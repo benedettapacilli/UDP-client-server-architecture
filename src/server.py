2703cd7757586f54c33b0d89887b5a523eab630c
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-05-31 17:51:27 +0200

    Completed server socket

--- src/server.py
+++ src/server.py
@@ -3,14 +3,15 @@ import time
 
 socket = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
 server_address = ('localhost', 10000)
-print('Starting up on porn %s \n', server_address)
+print('Starting up on {server_address[0]}, port {server_address[1]}')
 socket.bind(server_address)
+print('Now listening...\n')
 
 while True:
     print('Waiting to receive message...\n')
     data, address = socket.recvfrom(4096)
 
-    print('Received %s bytes from %s', len(data), address)
-    print(data.decode('utf8'))
+    print('Received %s bytes from %s' % (len(data), address))
+    message = data.decode('utf8')
 
     
