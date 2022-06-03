7bcb4de339610dd90754fa00bbdb87129f61ab78
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-06-03 18:51:52 +0200

    Completed libraries

---
+++ src/client/client_library.py
@@ -0,0 +1,57 @@
+import socket 
+import os
+
+BUFFERSIZE = 4096
+EOF = b' /EOF/ \r\n/'
+
+def list(socket, server_address, op):
+    print('Listing files...')
+    socket.sendto(op.encode(), server_address)
+    data, address = socket.recvfrom(BUFFERSIZE)
+    file_num = int(data.decode())
+    if file_num == 0:
+        print('No files found')
+    for i in range(file_num):
+        data, address = socket.recvfrom(BUFFERSIZE)
+        print(data.decode())
+
+def get(socket, server_address, op):
+    socket.sendto(op.encode(), server_address)
+    data, address = socket.recvfrom(BUFFERSIZE)
+    file_check = data.decode()
+    if file_check == 'no':
+        print('File not found')
+    else:
+        file_name = op.split()[1]
+        print('File found, downloading...')
+        with open('./client_files/' + file_name, 'wb') as f:
+            while True:
+                data, address = socket.recvfrom(BUFFERSIZE)
+                if data == EOF:
+                    f.close()
+                    print('File downloaded')
+                    break
+                f.write(data)
+
+def put(socket, server_address, op):
+    socket.sendto(op.encode(), server_address)
+    file_name = op.split()[1]
+    if os.path.exists('./client_files/' + file_name):
+        print('File found, uploading...')
+        with open('./client_files/' + file_name, 'rb') as f:
+            while True:
+                data = f.read(BUFFERSIZE)
+                if not data:
+                    socket.sendto(EOF, server_address)
+                    f.close()
+                    break
+                socket.sendto(data, server_address)
+        print('File uploaded')
+    else:
+        print('File not found')
+    
+def end_process(socket, server_address, op):
+    socket.sendto(op.encode(), server_address)
+    socket.close()
+    print('Exiting...')
+    exit()
--- src/server/server_library.py
+++ src/server/server_library.py
@@ -1,6 +1,43 @@
 import socket
-buffersize = 4096
+import os
 
-def get():
-    socket.recvfrom(buffersize)
+BUFFERSIZE = 4096
+EOF = b' /EOF/ \r\n/'
 
+def list(socket, server_address, op):
+    file_list = os.listdir('./server_files')
+    socket.sendto(str(len(file_list)).encode(), server_address)
+    for file in file_list:
+        socket.sendto(file.encode(), server_address)
+
+def send(socket, server_address, op):
+    file_name = op.split()[1]
+    if os.path.exists('./server_files/' + file_name):
+        socket.sendto('yes'.encode(), server_address)
+        with open('./server_files/' + file_name, 'rb') as f:
+            while True:
+                data = f.read(BUFFERSIZE)
+                if not data:
+                    socket.sendto(EOF, server_address)
+                    f.close()
+                    break
+                socket.sendto(data, server_address)
+        print('File sent')
+    else:
+        socket.sendto('no'.encode(), server_address)
+
+def receive(socket, op):
+    file_name = op.split()[1]
+    with open('./server_files/' + file_name, 'wb') as f:
+        while True:
+            data, addr = socket.recvfrom(BUFFERSIZE)
+            if data == EOF:
+                f.close()
+                break
+            f.write(data)
+    print('File received')
+
+def end_process(socket):
+    socket.close()
+    print('Exiting...')
+    exit()
